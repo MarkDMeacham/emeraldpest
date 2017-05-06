d60e15c8e7501eabb02716e57336cfac12cba04f
Mark Meacham <Marxmus@Marks-MacBook-Pro.local>
2017-05-06 11:21:48 -0600

    Root no longer routes to under construction

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # root 'welcome#index'
-  root 'welcome#construction'
+  root 'welcome#index'
+  # root 'welcome#construction'
   resources :welcome, only: [:index]
   resources :quotes, only: [:create, :new, :index]
 end
