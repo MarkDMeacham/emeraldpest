305d805e4f5da21e18fe7fafe7a46e1e0b344a5e
Mark Meacham <Marxmus@Marks-MacBook-Pro.local>
2017-05-06 11:57:13 -0600

    Adds sitemap

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # root 'welcome#construction'
   resources :welcome, only: [:index]
   resources :quotes, only: [:create, :new, :index]
+
+  get "sitemap.xml" => "sitemaps#index", :format => "xml", :as => :sitemap
 end
