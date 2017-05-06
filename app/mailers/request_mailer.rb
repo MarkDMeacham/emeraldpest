8758e4bb6732c086cd6f5c2d1c09aad247dc6cd1
Mark Meacham <Marxmus@Marks-MacBook-Pro.local>
2017-05-06 10:58:33 -0600

    Adjusting carousel image sizes. Changes email to go to owner

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -1,6 +1,6 @@
 class RequestMailer < ApplicationMailer
   def quote(request)
     @request = request
-    mail(to: "mark_meacham@hotmail.com", subject: "Quote Request")
+    mail(to: "emeraldpest@gmail.com", subject: "Quote Request")
   end
 end
